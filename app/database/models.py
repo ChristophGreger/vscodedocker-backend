c7b8cde8a9a8cdebc29d76aa8d3e5bae56cd10bf
ChristophGreger <mail@christoph-greger.de>
2024-04-09 16:01:31 +0100

    added database stuff

---
+++ app/database/models.py
@@ -0,0 +1,54 @@
+from app import db
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+
+class Volume(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(255), nullable=False, unique=True, index=True)
+
+    containers: so.WriteOnlyMapped["Container"] = so.relationship(back_populates="volume")
+
+    def __repr__(self):
+        return f'<Volume {self.name}>'
+
+
+class Vorlage(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(255), nullable=False, unique=True, index=True)
+    version: so.Mapped[int] = so.mapped_column(sa.Integer, nullable=False, index=True)
+    description: so.Mapped[str] = so.mapped_column(sa.String(255), nullable=False)
+    vscodeextension: so.Mapped[str] = so.mapped_column(sa.String(255), nullable=False)
+    installcommands: so.Mapped[str] = so.mapped_column(sa.String(255), nullable=False)
+
+    images: so.WriteOnlyMapped["Image"] = so.relationship(back_populates="vorlage")
+
+    def __repr__(self):
+        return f'<Vorlage {self.name}>'
+
+
+class Image(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(255), nullable=False, unique=True, index=True)
+    version: so.Mapped[int] = so.mapped_column(sa.Integer, nullable=False, index=True)
+    id_vorlage: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Vorlage.id), nullable=False, index=True)
+
+    containers: so.WriteOnlyMapped["Container"] = so.relationship(back_populates="image")
+    vorlage: so.Mapped[Vorlage] = so.relationship(back_populates="images")
+
+    def __repr__(self):
+        return f'<Image {self.name}>'
+
+
+class Container(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(255), nullable=False, unique=True, index=True)
+    port: so.Mapped[int] = so.mapped_column(sa.Integer, nullable=False)
+    id_volume: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Volume.id), nullable=False, index=True)
+    id_image: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Image.id), nullable=False, index=True)
+
+    volume: so.Mapped[Volume] = so.relationship(back_populates="containers")
+    image: so.Mapped[Image] = so.relationship(back_populates="containers")
+
+    def __repr__(self):
+        return f'<Container {self.name}>'
